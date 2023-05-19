c19d394647906270ff6c3fb624f7ee54febf1564
John16Mark <92175973+John16Mark@users.noreply.github.com>
2023-05-19 09:53:49 -0600

    Add files via upload

--- Practica2.cpp
+++ Practica2.cpp
@@ -6,7 +6,7 @@
 #include <regex>
 
 /*
-g++ Practica2.cpp -o Practica2.exe
+g++ Practica2.cpp -o Practica2.exe -std=c++11
 */
 
 using namespace std;
@@ -19,14 +19,23 @@ int main() {
 
     vector<string> tokens;
 
-    regex enteroDecimal("([0-9]+)");
-    regex enteroOctal("(0+[0-7]+)");
-	regex enteroHex("(0+x+[0-9a-fA-F]+)");
+    regex enteroDecimal("([+-]?([0-9]+)[,]?)");
+    regex enteroOctal("([+-]?([0-7]+)[,]?)");
+	regex enteroHex("(([+-]?0x[0-9a-fA-F])[,]?)");
+	regex realDecimal("([+-]?([0-9]+).([0-9]+)[,]?)");
+	regex realDecimalExp("([+-]?([0-9]+).([0-9]+)[,]?)");
+	
+	string intDec = "[+-]?([0-9]+)";
+	string intOct = "[+-]?([0-7]+)";
+	string intHex = "[+-]?0x[0-9a-fA-F]";
+	string realDec = "[+-]?([0-9]+).([0-9]+)";
+	string realDecExp = "[+-]?([0-9]+).([0-9]+)E[+-]?([0-9]+)";
 
     string linea;
     while (getline(archivo, linea)) {
         istringstream iss(linea);
         string palabra;
+        contador_linea++;
         while (iss >> palabra) {
             // Verificar si la palabra es una palabra clave
             /*if (palabra == "if" || palabra == "else" || palabra == "for" || palabra == "while") {
@@ -48,20 +57,22 @@ int main() {
             		cout << "Error: Línea " << contador_linea << endl;
             	}
             }*/
-            bool anteriorInt = false;
-            if (anteriorInt){
-            	if(!palabra =",")
-            }
-            else{
-            	if(palabra == "int"){
-	            	anteriorInt = true;
-	            }
-            }
+			bool ant_tipo_dato = false;
+			if(ant_tipo_dato){
+				if(palabra == "int" || palabra == "short" || palabra == "double" || palabra == "float"){
+					cout << "Error: Línea " << contador_linea << endl; 
+				}else{
+					
+				}
+			}
+			else{
+				if()
+			}
             
-
+			
         }
     }
-
+/*
     while(pilaIF.empty()){
     	cout << "Error: Línea " << contador_linea << endl;
     	pilaIF.pop();
@@ -71,6 +82,12 @@ int main() {
     for (const string& token : tokens) {
         cout << token << endl;
     }
-
+*/
     return 0;
 }
+
+bool check_dato(string p){
+	
+}
+
+bool check_tipo_dato
