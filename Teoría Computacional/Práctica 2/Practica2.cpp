a24b0146090c5a002e4512077802f80973d9d3cb
John16Mark <92175973+John16Mark@users.noreply.github.com>
2023-05-28 17:51:04 -0600

    Cambios

--- Teoría Computacional/Práctica 2/Practica2.cpp
+++ Teoría Computacional/Práctica 2/Practica2.cpp
@@ -3,12 +3,24 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <deque>
 #include "automata.h"
-#include <locale.h>
+#include <windows.h>  
 
 /*
 g++ Practica2.cpp -o Practica2.exe automata.cpp
 */
+
+//bool comentarioMulti = false;
+
+void gotoxy(int x, int y)
+{
+    COORD coordinate;
+    coordinate.X = x;
+    coordinate.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinate);
+}
 
 typedef struct celda{
     string pal;
@@ -16,8 +28,12 @@ typedef struct celda{
     Estado tipo;
 }celda;
 
+vector<int> errores;
 
 using namespace std;
+
+void tabla(vector<celda> *a);
+vector<string> dividir(const string& s, char delimitador);
 
 int main() {
 
@@ -25,6 +41,7 @@ int main() {
     int contador_linea = 0;
 
     vector<celda> analizador;
+    
 
     string linea;
     while (getline(archivo, linea)) {
@@ -32,19 +49,164 @@ int main() {
         istringstream iss(linea);
         string palabra;
         celda celda_aux;
+
         while (iss >> palabra) {
-            celda_aux.pal = palabra;
-            celda_aux.linea = contador_linea;
-            celda_aux.tipo = verificarPalabra(palabra);
-            analizador.push_back(celda_aux);
-            //cout << palabra << "\t\t";
-            //cout << imprimirEstado(verificarPalabra(palabra)) << endl;
+            
+            deque<string> cola_despues;
+
+            cout << "\n    SE RECIBIO LA PALABRA: " <<palabra << endl;
+            vector<string> partes = dividir(palabra, '(');
+            vector<string> partes2;
+
+            for (const string& parte : partes) {
+                vector<string> aux = dividir(parte, '{');
+                for(int i = 0; i < aux.size(); i++) {
+                    partes2.push_back(aux.at(i));
+                }
+            }
+            partes.clear();
+
+            for (const string& parte : partes2) {
+                vector<string> aux = dividir(parte, ')');
+                for(int i = 0; i < aux.size(); i++) {
+                    partes.push_back(aux.at(i));
+                }
+            }
+            partes2.clear();
+
+            for (const string& parte : partes) {
+                vector<string> aux = dividir(parte, '[');
+                for(int i = 0; i < aux.size(); i++) {
+                    partes2.push_back(aux.at(i));
+                }
+            }
+            partes.clear();
+
+            for (const string& parte : partes2) {
+                vector<string> aux = dividir(parte, ']');
+                for(int i = 0; i < aux.size(); i++) {
+                    partes.push_back(aux.at(i));
+                }
+            }
+
+            //
+            for (const string& parte : partes) {
+                celda_aux.pal = parte;
+                celda_aux.linea = contador_linea;
+                celda_aux.tipo = verificarPalabra(parte);
+                analizador.push_back(celda_aux);
+            }
+
+            
+            
+        }
+    }
+    tabla(&analizador);
+    
+
+    return 0;
+}
+
+vector<string> dividir(const string& s, char delimitador){
+    vector<string> partes;
+    string parte;
+
+    for(char c: s) {
+        if ( c == delimitador) {
+            if(!parte.empty()) {
+                partes.push_back(parte);
+                parte.clear();
+            }
+            partes.push_back(string(1, delimitador));
+        } else {
+            parte += c;
         }
     }
 
-    for(celda cel: analizador){
-        cout << cel.pal << "\t\t" << imprimirEstado(cel.tipo) << endl;
+    if (!parte.empty()) {
+        partes.push_back(parte);
     }
 
-    return 0;
+    return partes;
+}
+
+void tabla(vector<celda> *a){
+    int i;
+    system("CLS");
+    gotoxy(4,2);
+    printf("%c",201);
+    for(i = 0; i < 85; i++){
+        printf("%c",205);
+    }
+    printf("%c",187);
+    gotoxy(4,3);
+    printf("%c",186);
+    gotoxy(90,3);
+    printf("%c",186);
+    gotoxy(38,3);
+    printf("TABLA DE PALABRAS");
+    gotoxy(4,4);
+    printf("%c",204);
+    for(i = 0; i < 47; i++){
+        printf("%c",205);
+    }
+    printf("%c",203);
+    for(i = 0; i < 9; i++){
+        printf("%c",205);
+    }
+    printf("%c",203);
+    for(i = 0; i < 27; i++){
+        printf("%c",205);
+    }
+    printf("%c",185);
+    gotoxy(4,5);
+    printf("%c Palabra",186);
+    gotoxy(52,5);
+    printf("%c Rengl%cn",186,162);
+    gotoxy(62,5);
+    printf("%c Tipo",186);
+    gotoxy(90,5);
+    printf("%c",186);
+
+    gotoxy(4,6);
+    printf("%c",200);
+    for(i = 0; i < 47; i++){
+        printf("%c",205);
+    }
+    printf("%c",202);
+    for(i = 0; i < 9; i++){
+        printf("%c",205);
+    }
+    printf("%c",202);
+    for(i = 0; i < 27; i++){
+        printf("%c",205);
+    }
+    printf("%c",188);
+
+    int count = 0;
+    for(celda cel: *a){
+        gotoxy(4,7+count);
+        cout << (char)179 << " " <<cel.pal;
+        gotoxy(52,7+count);
+        cout << (char)179 << " " << cel.linea;
+        gotoxy(62,7+count);
+        cout << (char)179 << " " << imprimirEstado(cel.tipo);
+        gotoxy(90,7+count);
+        cout << (char)179;
+        count++;
+    }
+    gotoxy(4,7+count);
+    printf("%c",192);
+    for(i = 0; i < 47; i++){
+        printf("%c",196);
+    }
+    printf("%c",193);
+    for(i = 0; i < 9; i++){
+        printf("%c",196);
+    }
+    printf("%c",193);
+    for(i = 0; i < 27; i++){
+        printf("%c",196);
+    }
+    printf("%c",217);
 }
--- Teoría Computacional/Práctica 2/automata.cpp
+++ Teoría Computacional/Práctica 2/automata.cpp
@@ -4,213 +4,267 @@
 
 using namespace std;
 
+bool comentarioMulti = false;
+
 Estado verificarPalabra(const string& palabra){
 
 	Estado estado = Estado::INICIAL;
 	//bool imprimir = true;
-	for(char c: palabra){
-		switch (estado){
-			case Estado::INICIAL:
-				if(c == '0') {
-					estado = Estado::OCT_HEX;
-					//if(imprimir) {printf("  \033[96mOCT_HEX\033[0m");}  //--------------------------------------
-				} else if(c == '+' || c == '-') {
-					estado = Estado::SIGNO;
-				} else if(c == '%') {
-					estado = Estado::OPERADOR_BINARIO;
-				} else if(c == '/') {
-					estado = Estado::COMENTARIOS_DIVISION;
-				} else if(c == '*') {
-					estado = Estado::COMENTARIOS_MULTIPLICACION;
-				} else if(c == '=') {
-					estado = Estado::ASIGNADOR;
-				} else if(c == '{') {
-					estado = Estado::CORCHETES_APERTURA;
-				} else if(c == '}') {
-					estado = Estado::CORCHETES_CERRADURA;
-				} else if(c >= '1' && c <= '9') {
-					estado = Estado::DECIMAL;
-				} else if(isalpha(c) || c == '_' || c == '$') {
-					estado = Estado::ID_RESERVADA;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::SIGNO:
-				if (c == '0'){
-					estado = Estado::OCT_HEX;
-				} else if(c >= '1' && c <= '9') {
-					estado = Estado::DECIMAL;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::OCT_HEX:
-				if(c == 'x'){
-					estado = Estado::HEX;
-				} else if(c >= '0' && c <= '7'){
-					estado = Estado::OCTAL;
-				} else if(c == '.') {
-					estado = Estado::REAL;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::OCTAL:
-				if(c >= '0' && c <= '7'){
-					estado = Estado::OCTAL;
-				} else if(c == ',') {
-					estado = Estado::OCTAL_COMA;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::HEX:
-				if((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F')){
-					estado = Estado::HEX_DIGITO;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::HEX_DIGITO:
-				if((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F')){
-					estado = Estado::HEX_DIGITO;
-				} else if (c == ',') {
-					estado = Estado::HEX_COMA;
-				} else if (c == ';') {
-					estado = Estado::HEX_PCOMA;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::DECIMAL:
-				if (c >= '0' && c <= '9') {
-					estado = Estado::DECIMAL;
-				} else if (c == '.') {
-					estado = Estado::PUNTO;
-				} else if (c == ',') {
-					estado = Estado::DECIMAL_COMA;
-				} else if (c == ';') {
-					estado = Estado::DECIMAL_PCOMA;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::PUNTO:
-				if (c >= '0' && c <= '9') {
-					estado = Estado::REAL;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::REAL:
-				if (c >= '0' && c <= '9') {
-					estado = Estado::REAL;
-				} else if (c == 'E') {
-					estado = Estado::EXP; 
-				} else if (c == ',') {
-					estado = Estado::DECIMAL_COMA;
-				} else if (c == ';') {
-					estado = Estado::DECIMAL_PCOMA;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::EXP:
-				if(c == '+' || c == '-') {
-					estado = Estado::EXP_SIGNO;
-				} else if (c >= '0' && c <= '9') {
-					estado = Estado::EXP_REAL;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::EXP_SIGNO:
-				if (c >= '0' && c <= '9') {
-					estado = Estado::EXP_REAL;
-				} else {
-					estado = Estado::INVALIDO;
-					return estado;
-				}
-				break;
-			case Estado::EXP_REAL:
-				if (c >= '0' && c <= '9') {
-					estado = Estado::EXP_REAL2;
-				} else if (c == ',') {
-					estado = Estado::DECIMAL_COMA;
-				} else if (c == ';') {
-					estado = Estado::DECIMAL_PCOMA;
-				} else {
-					estado = Estado::INVALIDO;
-					return estado;
-				}
-				break;
-			case Estado::EXP_REAL2:
-				if (c == ',') {
-					estado = Estado::DECIMAL_COMA;
-				} else if (c == ';') {
-					estado = Estado::DECIMAL_PCOMA;
-				}
-				break;
-			case Estado::OCTAL_COMA:
-			case Estado::OCTAL_PCOMA:
-			case Estado::DECIMAL_COMA:
-			case Estado::DECIMAL_PCOMA:
-			case Estado::HEX_COMA:
-			case Estado::HEX_PCOMA:
-				estado = Estado::INVALIDO;
-				break;
-			case Estado::ID_RESERVADA:
-				if(palabra == "public" || palabra == "private" || palabra == "package" || palabra == "protected" ||
-					palabra == "static" || palabra == "final" || palabra == "class") {
-					estado = Estado::RESERVADA;
-					return estado;
-				} else if(palabra == "int" || palabra == "short" || palabra == "void" || palabra == "float" ||
-					palabra == "double") {
-					estado = Estado::TIPO_DATO;
-					return estado;
-				} else {
-					if (c == '_' || isalpha(c) || isdigit(c)){
-						estado = Estado::IDENTIFICADOR;
+	if(comentarioMulti && palabra.at(0) != '*'){
+		return Estado::COMENTARIO;
+	} else {
+		for(char c: palabra){
+			switch (estado){
+				case Estado::INICIAL:
+					if(c == '0') {
+						estado = Estado::OCT_HEX;
+						//if(imprimir) {printf("  \033[96mOCT_HEX\033[0m");}  //--------------------------------------
+					} else if(c == '+' || c == '-') {
+						estado = Estado::SIGNO;
+					} else if(c == '%') {
+						estado = Estado::OPERADOR_BINARIO;
+					} else if(c == '/') {
+						estado = Estado::COMENTARIOS_DIVISION;
+					} else if(c == '*') {
+						estado = Estado::COMENTARIOS_MULTIPLICACION;
+					} else if(c == '=') {
+						estado = Estado::ASIGNADOR;
+					} else if(c == '{') {
+						estado = Estado::LLAVES_APERTURA;
+					} else if(c == '}') {
+						estado = Estado::LLAVES_CERRADURA;
+					} else if(c == '[') {
+						estado = Estado::CORCHETES_APERTURA;
+					} else if(c == ']') {
+						estado = Estado::CORCHETES_CERRADURA;
+					} else if(c == '(') {
+						estado = Estado::PARENTESIS_APERTURA;
+					} else if(c == ')') {
+						estado = Estado::PARENTESIS_CERRADURA;
+					} else if(c == '<' || c == '>'){
+						estado = Estado::COMPARADOR_1;
+					} else if(c >= '1' && c <= '9') {
+						estado = Estado::DECIMAL;
+					} else if(isalpha(c) || c == '_' || c == '$') {
+						estado = Estado::ID_RESERVADA;
 					} else {
 						estado = Estado::INVALIDO;
 					}
-				}
-				break;
-			case Estado::IDENTIFICADOR:
-				if (c == '_' || isalpha(c) || isdigit(c)){
-					estado = Estado::IDENTIFICADOR;
-				} else if(c == ','){
-					estado =Estado::ID_COMA;
-				} else if(c == ';'){
-					estado =Estado::ID_PCOMA;
-				} else {
+					break;
+				case Estado::SIGNO:
+					if (c == '0'){
+						estado = Estado::OCT_HEX;
+					} else if(c >= '1' && c <= '9') {
+						estado = Estado::DECIMAL;
+					} else if(c == '=') {
+						estado = Estado::ASIGNADOR;
+					} else {
+						estado = Estado::INVALIDO;
+					}
+					break;
+				case Estado::OCT_HEX:
+					if(c == 'x'){
+						estado = Estado::HEX;
+					} else if(c >= '0' && c <= '7'){
+						estado = Estado::OCTAL;
+					} else if(c == '.') {
+						estado = Estado::REAL;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+					}
+					break;
+				case Estado::OCTAL:
+					if(c >= '0' && c <= '7'){
+						estado = Estado::OCTAL;
+					} else if(c == ',') {
+						estado = Estado::OCTAL_COMA;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+					}
+					break;
+				case Estado::HEX:
+					if((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F')){
+						estado = Estado::HEX_DIGITO;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+					}
+					break;
+				case Estado::HEX_DIGITO:
+					if((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F')){
+						estado = Estado::HEX_DIGITO;
+					} else if (c == ',') {
+						estado = Estado::HEX_COMA;
+					} else if (c == ';') {
+						estado = Estado::HEX_PCOMA;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+					}
+					break;
+				case Estado::DECIMAL:
+					if (c >= '0' && c <= '9') {
+						estado = Estado::DECIMAL;
+					} else if (c == '.') {
+						estado = Estado::PUNTO;
+					} else if (c == ',') {
+						estado = Estado::DECIMAL_COMA;
+					} else if (c == ';') {
+						estado = Estado::DECIMAL_PCOMA;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+					}
+					break;
+				case Estado::PUNTO:
+					if (c >= '0' && c <= '9') {
+						estado = Estado::REAL;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+					}
+					break;
+				case Estado::REAL:
+					if (c >= '0' && c <= '9') {
+						estado = Estado::REAL;
+					} else if (c == 'E') {
+						estado = Estado::EXP; 
+					} else if (c == ',') {
+						estado = Estado::DECIMAL_COMA;
+					} else if (c == ';') {
+						estado = Estado::DECIMAL_PCOMA;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+					}
+					break;
+				case Estado::EXP:
+					if(c == '+' || c == '-') {
+						estado = Estado::EXP_SIGNO;
+					} else if (c >= '0' && c <= '9') {
+						estado = Estado::EXP_REAL;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+					}
+					break;
+				case Estado::EXP_SIGNO:
+					if (c >= '0' && c <= '9') {
+						estado = Estado::EXP_REAL;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+						return estado;
+					}
+					break;
+				case Estado::EXP_REAL:
+					if (c >= '0' && c <= '9') {
+						estado = Estado::EXP_REAL2;
+					} else if (c == ',') {
+						estado = Estado::DECIMAL_COMA;
+					} else if (c == ';') {
+						estado = Estado::DECIMAL_PCOMA;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+						return estado;
+					}
+					break;
+				case Estado::EXP_REAL2:
+					if (c == ',') {
+						estado = Estado::DECIMAL_COMA;
+					} else if (c == ';') {
+						estado = Estado::DECIMAL_PCOMA;
+					} else {
+						estado = Estado::ERROR_NUMERICO;
+						return estado;
+					}
+					break;
+				case Estado::OCTAL_COMA:
+				case Estado::OCTAL_PCOMA:
+				case Estado::DECIMAL_COMA:
+				case Estado::DECIMAL_PCOMA:
+				case Estado::HEX_COMA:
+				case Estado::HEX_PCOMA:
 					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::COMENTARIOS_DIVISION:
-				if (c == '/') {
-					estado = Estado::COMENTARIO_SIMPLE;
-				} else if (c == '*') {
-					estado = Estado::COMENTARIO_MULTILINEA;
-				} else {
+					break;
+				case Estado::ID_RESERVADA:
+					if(palabra == "public" || palabra == "private" || palabra == "package" || palabra == "protected" ||
+						palabra == "static" || palabra == "final" || palabra == "class") {
+						estado = Estado::RESERVADA;
+						return estado;
+					} else if(palabra == "int" || palabra == "short" || palabra == "void" || palabra == "float" ||
+						palabra == "double" || palabra == "String") {
+						estado = Estado::TIPO_DATO;
+						return estado;
+					} else {
+						if (c == '_' || isalpha(c) || isdigit(c)){
+							estado = Estado::IDENTIFICADOR;
+						} else {
+							estado = Estado::INVALIDO;
+						}
+					}
+					break;
+				case Estado::IDENTIFICADOR:
+					if (c == '_' || isalpha(c) || isdigit(c)){
+						estado = Estado::IDENTIFICADOR;
+					} else if(c == ','){
+						estado =Estado::ID_COMA;
+					} else if(c == ';'){
+						estado =Estado::ID_PCOMA;
+					} else {
+						estado = Estado::INVALIDO;
+					}
+					break;
+				case Estado::COMENTARIOS_DIVISION:
+					if (c == '/') {
+						estado = Estado::COMENTARIO_SIMPLE;
+					} else if (c == '*') {
+						estado = Estado::COMENTARIO_MULTILINEA;
+						comentarioMulti = true;
+					} else {
+						estado = Estado::INVALIDO;
+					}
+					break;
+				case Estado::COMENTARIOS_MULTIPLICACION:
+					if (c == '/') {
+						estado = Estado::COMENTARIO_MULTILINEA_FIN;
+						comentarioMulti = false;
+					} else {
+						estado = Estado::INVALIDO;
+					}
+					break;
+				case Estado::COMENTARIO_SIMPLE:
+				case Estado::COMENTARIO_MULTILINEA:
+					
+					break;
+				case Estado::ASIGNADOR:
+					if (c == '=') {
+						estado = Estado::COMPARADOR_2;
+					} else {
+						estado = Estado::INVALIDO;
+					}
+					break;
+				case Estado::COMPARADOR_1:
+					if(c == '=') {
+						estado = Estado::COMPARADOR_2;
+					} else {
+						estado = Estado::INVALIDO;
+					}
+					break;
+				case Estado::COMENTARIO:
+					if(c == '*'){
+						estado = Estado::COMENTARIO_SLASH;
+					} else {
+						Estado::COMENTARIO;
+					}
+					break;
+				case Estado::COMENTARIO_SLASH:
+					if (c == '/') {
+						estado = Estado::COMENTARIO_MULTILINEA_FIN;
+						comentarioMulti = false;
+					} else {
+						Estado::COMENTARIO;
+					}
+					break;
+				case Estado::ERROR_NUMERICO:
+					break;
+				default:
 					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::COMENTARIOS_MULTIPLICACION:
-				if (c == '/') {
-					estado = Estado::COMENTARIO_MULTILINEA_FIN;
-				} else {
-					estado = Estado::INVALIDO;
-				}
-				break;
-			case Estado::COMENTARIO_SIMPLE:
-			case Estado::COMENTARIO_MULTILINEA:
-				
-				break;
-			default:
-				estado = Estado::INVALIDO;
-				break;
+					break;
+			}
 		}
 	}
 
@@ -225,6 +279,9 @@ Estado verificarPalabra(const string& palabra){
 
 string imprimirEstado(Estado e){
 	switch(e){
+	case Estado::ERROR_NUMERICO:
+		return "\033[91mERROR NUMERICO\033[0m";
+		break;
 	case Estado::INVALIDO:
 		return "\033[31mINVALIDO\033[0m";
 		break;
@@ -282,8 +339,21 @@ string imprimirEstado(Estado e){
 	case Estado::CORCHETES_CERRADURA:
 		return "Corchetes: Cerrar";
 		break;
+	case Estado::PARENTESIS_APERTURA:
+		return "Parentesis: Abrir";
+		break;
+	case Estado::PARENTESIS_CERRADURA:
+		return "Parentesis: Cerrar";
+		break;
+	case Estado::LLAVES_APERTURA:
+		return "Llaves: Abrir";
+		break;
+	case Estado::LLAVES_CERRADURA:
+		return "Llaves: Cerrar";
+		break;
 	default:
 		return "\033[90m\033[4mPOR IDENTIFICAR\033[0m";
 		break;
 	}
 }
+
